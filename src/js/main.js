bc7917cb8e6a60cc77f5365cd0200c9b29425e82
petraelgemyr <petra.elgemyr@gmail.com>
2022-11-04 17:02:30 +0100

    tried sort btn

--- src/js/main.js
+++ src/js/main.js
@@ -5,6 +5,8 @@ window.onload = function () {
   showMyTodos();
 };
 
+let sortBtn = document.getElementById("sortBtn");
+
 let removedTodoList = [];
 let todoList = [
   new Todo("Tvätta", false, false),
@@ -12,6 +14,11 @@ let todoList = [
   new Todo("Plugga", false, false),
   new Todo("Städa", false, false),
 ];
+
+sortBtn.addEventListener("click", () => {
+  todoList.sort((a, b) => (a.name > b.name ? 1 : -1)); //sorterar alfabetiskt men bara i todoList än så länge
+  showMyTodos();
+});
 
 function addItemToList() {
   let addItemBtn = document.getElementById("addItemBtn");
@@ -114,9 +121,11 @@ function checkingCheckbox(myLiTag, clickedObject, checkbox, mySpan) {
       }
 
       ulWithRemovedItems.appendChild(removedLi);
-      removedSpan.innerHTML += clickedObject.name;
-      removedLi.appendChild(removedSpan);
-      removedLi.appendChild(checkboxForRemoved);
+      for (let i = 0; i < removedTodoList.length; i++) {
+        removedSpan.innerHTML = clickedObject.name;
+        removedLi.appendChild(removedSpan);
+        removedLi.appendChild(checkboxForRemoved);
+      }
 
       removedSpan.addEventListener("click", () => {
         //om man klickar på span i den borttagna listan, ska få överstruken styling och byta finished-värde
@@ -173,293 +182,12 @@ function reverse(
   clickedObject.removed = false; //när objekt flyttats över till todolistan ska objektets removed=false OCH checkrutan ska räknas som icke-checkad
   myLiTag.appendChild(firstCheckbox); //checkbox för todo-listan är tillbaka
 
+  if (clickedObject.finished === true) {
+    myLiTag.classList.toggle("__removed");
+  }
+
   let myUlTag = document.getElementById("myList");
   myUlTag.appendChild(myLiTag);
   console.log("min uppdaterade todo-list: ", todoList);
   console.log("min uppdaterade removed-list: ", removedTodoList);
 }
-
-// function markAsFinished(theObject, myLiTag, removedLi) { //om man klickat på span-tagen
-//   if (theObject.finished === false) {
-//     theObject.finished = true; //om finished=false (dvs standardvärdet) så ska det växla till true vid klick på span
-//     console.log("Nu blir finished=true på objektet ", theObject);
-//     myLiTag.classList.toggle("__checked");
-//     removedLi.classList.toggle("__checked");
-//   } else {
-//     if (theObject.finished === true) {
-//       //om finished=true när man klickar ska det bli false
-//       theObject.finished = false;
-//       console.log("Nu blir finished=false på objektet ", theObject);
-//       myLiTag.classList.toggle("__checked");
-//       removedLi.classList.remove("__checked");
-//     }
-//   }
-// }
-
-/********************************************************* 
-
-  totalTodoList.push(newTodoObject); //här läggs det till objekt newTodoObject i min lista [newTodoObject, newTodoObject,...]
-  if (content !== "") {
-    console.log(totalTodoList);
-    // let ulTag = document.getElementById("myList");
-    // ulTag.innerHTML = "";
-
-    for (let i = 0; i < totalTodoList.length; i++) {
-      myLiTag.innerHTML = "";
-      myLiTag.innerHTML = totalTodoList[i].item;
-      let checkbox = document.createElement("input");
-      checkbox.type = "checkbox"; //Om man klickar på checkbox för att markera klar
-
-      let removeItemBtn = document.createElement("button");
-      removeItemBtn.type = "button";
-      removeItemBtn.className = "removeBtn";
-      removeItemBtn.innerHTML = "&#10005;";
-
-      myLiTag.appendChild(checkbox);
-      myLiTag.appendChild(removeItemBtn);
-      myUlTag.appendChild(myLiTag);
-
-      checkbox.addEventListener("change", () => {
-        if (checkbox.checked) {
-          totalTodoList[i].finished = true;
-          myLiTag.classList.toggle("__checked");
-          console.log(
-            "Todon ",
-            totalTodoList[i].item,
-            " är klar: ",
-            totalTodoList[i].finished
-          );
-        } else {
-          if (!checkbox.checked) {
-            totalTodoList[i].finished = false;
-            console.log(
-              "Todon ",
-              totalTodoList[i].item,
-              " är klar: ",
-              totalTodoList[i].finished
-            );
-            myLiTag.className = "myTodoItem";
-          }
-        }
-      });
-
-      removeItemBtn.addEventListener("click", () => {
-        handleRemovedItems(totalTodoList[i], totalTodoList, myLiTag, checkbox);
-      });
-    }
-******************************************/
-
-/**
-   * 
-   * 
-  btn.addEventListener("click", function () {
-        todo.removed = true;
-  
-        let removedItem = btn.parentElement;
-        removedItem.classList.toggle("checked");
-        checkChecked(todo);
-        removedItem.remove();
-        removedItem.removeChild(removedItem.lastChild);
-  
-        //Lägg till ny knapp som gör att man kan "reversa" en borttagning av en todo
-        let reverseBtn = document.createElement("button");
-        reverseBtn.innerHTML = "&#8617;";
-        reverseBtn.className = "reverseListItem";
-  
-        removedItem.appendChild(reverseBtn);
-  
-        removedItems.push(removedItem);
-  
-        // eventlistner för nya reverseknappen som gör att todon kommer tillbaka till hur den var innan den blev borttagen
-        reverseBtn.addEventListener("click", function () {
-          todo.removed = false;
-  
-          removedItem.classList.toggle("checked");
-          checkChecked(todo);
-  
-          let reverseItem = reverseBtn.parentElement;
-          reverseItem.removeChild(reverseItem.lastChild);
-  
-          reverseItem.appendChild(btn);
-  
-          reverseItem.classList.remove("removed");
-  
-          let index = removedItems.indexOf(reverseItem);
-          if (index > -1) {
-            removedItems.splice(index, 1);
-          } 
-  
-  
-  
-   * 
-   */
-
-// removeItemBtn.addEventListener("click", () => {
-//   newTodoObject.removed = true;
-//   let reverseBtn = document.createElement("button");
-//   if (newTodoObject.removed === true) {
-//     let deleteItem = removeItemBtn.parentElement; //här är den specifika knappens förälder, dvs den li-tagen
-//     deleteItem.remove(); //ta bort li-tag
-//     deleteItem.removeChild(deleteItem.lastChild); //tar bort kryssknapp
-//     console.log(deleteItem.innerHTML + " tas bort från todo-listan"); //skriver ut texten utan kryssknappen i console
-
-//     let newRemovedObjects = new Todo(deleteItem.innerText, false, true);
-
-//     removedTodoList.push(newRemovedObjects); //lägger till nya objektet av raderad punkt i listan med borttagna saker
-
-//     let liWithRemovedItems = document.createElement("li"); //skapa ny li-tag utanför loop, för varje klick skapas det ny li
-//     reverseBtn.type = "button";
-//     reverseBtn.innerHTML = "&#8617";
-
-//     for (let i = 0; i < removedTodoList.length; i++) {
-//       liWithRemovedItems.innerHTML = removedTodoList[i].item;
-//       liWithRemovedItems.appendChild(reverseBtn);
-//       ulWithRemovedItems.appendChild(liWithRemovedItems);
-//     }
-//     liWithRemovedItems.className = "deletedThing"; // Fixa så att styling o classname behålls från tidigare, något med true/false
-//     console.log(removedTodoList);
-//   } else {
-//     console.log(totalTodoList);
-//   }
-
-//   reverseBtn.addEventListener("click", () => {
-//     newTodoObject.removed = false;
-
-//     let bringBackItem = reverseBtn.parentElement; //li-tagen som ligger i borttagna saker
-//     bringBackItem.remove(); //li-tag tas bort från ul-listan
-//     bringBackItem.removeChild(bringBackItem.lastChild); //tar bort reverse button
-//     console.log(bringBackItem.innerHTML + " läggs tillbaka i todo-listan");
-//     let broughtBackItems = new Todo(bringBackItem.innerText, false, false);
-//     removedTodoList.pop(broughtBackItems); //ta bort sista objekt
-//     totalTodoList.push(broughtBackItems); //lägger till objektet på todo-list igen.
-
-//     console.log(removedTodoList);
-
-//     showMyTodos(bringBackItem.innerHTML); //skicka tillbaka texten på li-tagen som ska reversas
-// pop  let newTodoObject = new Todo(content, false, false);
-//   });
-// });
-
-/*******************************************************
-
-
-  } else {
-    alert("Skriv in något att lägga till innan du klickar!"); //Sorry, kommer bara avnvända alert här för att få fram en tydlig varning. Aldrig mer :)
-  }
-}
-function handleRemovedItems(clickedItem, totalTodoList, myLiTag, checkbox) {
-  clickedItem.removed = true;
-  console.log("Det här innehåller mitt objekt: ", Object.keys(clickedItem));
-
-  if (clickedItem.removed === true) {
-    let index = totalTodoList.indexOf(clickedItem);
-    console.log(index);
-    totalTodoList.splice(index, 1);
-    removedTodoList.push(clickedItem);
-    console.log(totalTodoList);
-    console.log(removedTodoList);
-    let todoListUlTag = document.getElementById("myList");
-    todoListUlTag.removeChild(myLiTag);
-
-    let removedListUl = document.getElementById("removedItemsList");
-    let removedLi = document.createElement("li");
-
-    if (checkbox.checked) {
-      clickedItem.finished = true;
-      removedLi.classList.toggle("__checked");
-    }
-
-    for (let i = 0; i < removedTodoList.length; i++) {
-      removedLi.innerHTML = "";
-      removedLi.className = "deletedLi";
-      removedLi.innerHTML = removedTodoList[i].item;
-
-      //   let checkbox = document.createElement("input");
-      //   checkbox.type = "checkbox";
-
-      let reverseBtn = document.createElement("button");
-      reverseBtn.type = "button";
-      reverseBtn.innerHTML = "&#8617";
-
-      removedLi.appendChild(checkbox);
-      removedLi.appendChild(reverseBtn);
-      removedListUl.appendChild(removedLi);
-
-************************************************/
-
-//   checkbox.addEventListener("change", () => {
-//     if (checkbox.checked) {
-//       totalTodoList[i].finished = true;
-//       myLiTag.classList.toggle("__checked");
-//       console.log(
-//         "Todon ",
-//         totalTodoList[i].item,
-//         " är klar: ",
-//         totalTodoList[i].finished
-//       );
-//     } else {
-//       if (!checkbox.checked) {
-//         totalTodoList[i].finished = false;
-//         console.log(
-//           "Todon ",
-//           totalTodoList[i].item,
-//           " är klar: ",
-//           totalTodoList[i].finished
-//         );
-//         myLiTag.className = "deletedLi";
-//       }
-//     }
-//   });
-
-/* ****************************************
-    }
-    let textStrings = [];
-    for (let i = 0; i < totalTodoList.length; i++) {
-      textStrings.push(totalTodoList[i].item);
-    }
-    console.log("mina texter", textStrings);
-
-    for (let i = 0; i < textStrings.length; i++) {
-      totalTodoList = [];
-
-      showMyTodos(textStrings[i]);
-    }
-
-    //const fruits = ["Banana", "Orange", "Apple", "Mango"]; Kolla upp dessa för att skicka item-namn till content i showmyTodos
-    //let text = fruits.toString();
-  }
-}
-************************************* */
-
-// function removeFromTodoList(myLiTag, clickedItem, totalTodoList) {
-//   if (totalTodoList[i].removed === false) {
-//     totalTodoList[i].removed = true;
-//     myLiTag.innerHTML = "";
-//     let index = totalTodoList.indexOf(totalTodoList[i]);
-//     console.log(index);
-//     totalTodoList.splice(index, 1);
-//     removedTodoList.push(clickedItem);
-
-//     for (let i = 0; i < ulWithRemovedItems.length; i++) {
-//       let reverseBtn = document.createElement("button");
-//       reverseBtn.type = "button";
-//       reverseBtn.innerHTML = "&#8617";
-//       let newLi = document.createElement("li");
-//       newLi.innerHTML = ulWithRemovedItems[i].name;
-//       newLi.appendChild(reverseBtn);
-//       ulWithRemovedItems.appendChild;
-
-//       // reverseBtn.addEventListener("click", )
-//     }
-//   }
-//   // else {
-//   //   if (totalTodoList[i].removed === true) {
-//   //     totalTodoList[i].removed = false;
-//   //     let removedItemsIndex =
-//   //   }
-//   // }
-// }
-
-/***********************
-
-********** */
